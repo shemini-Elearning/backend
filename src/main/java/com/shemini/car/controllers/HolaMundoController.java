4a7e50a81a94a06bb46172e30bfdf624f88c592f
Jhonny l Sarmiento <jhleonard26@gmail.com>
2023-05-06 23:41:40 -0500

    acrualizacion controler

--- src/main/java/com/shemini/car/controllers/HolaMundoController.java
+++ src/main/java/com/shemini/car/controllers/HolaMundoController.java
@@ -1,7 +1,16 @@
 package com.shemini.car.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HolaMundoControleler {
+@RequestMapping("/saludar")
+public class HolaMundoController {
+
+    @GetMapping("/hola")
+    public String saludar(){
+        return "<h1> Hola Mundo G2 </h1>";
+    }
+
 }
